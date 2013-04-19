dc7aef15f84941e5a133d1f4e8e2546eb102c59e
proposicion47 <proposicion47@gmail.com>
2013-04-18 21:18:19 -0500

    Update cuatroregiones.py
    fixing the ymax parameter on the P2 diagram

--- cuatroregiones.py
+++ cuatroregiones.py
@@ -8,8 +8,7 @@ def kronecker(l,m,n):
     return k
 
 def regionP1(l1,l2,mu1,mu2,mu3,nu1,nu2):
-  
-    
+      
     if (mu1 < mu2) or (mu2 < mu3):
         print("mu no es una partición")
     elif not(mu1+mu2+mu3 == nu1+nu2):
@@ -82,33 +81,28 @@ def regionP2(l1,l2,mu1,mu2,mu3,nu1,nu2):
         
         nu1b = nu1 +1
         
+        #REGION P2
+        g0 = max(0, l2-mu1)
+        g1 =min(l1-mu1, l2-mu2)
+        
         s= min(mu3,nu1b/3)
         q = min(nu1b+l1-mu1,nu1b+l2-mu2)
         
         xmax = 10
-        xmin=0
-        ymax=max(nu1-l2,(nu1-mu3)/2)+1
+        xmin=min(g0,g1)-1
+        ymax=max(nu1-l2,(nu1-mu3)/2,mu2,nu1/2)+1
         ymin=mu3-1
         ymin=0
-        
-        #ineqs = [d>=0, y>=0, y<= t1 ,d+y-mu2 <= 0,d+y-mu3 >= 0,2*y+d<=l2,  nu1+l2-mu1<= 3*y+d, 3*y+d <= t3, 4*y+d <= nu1+mu3]
-        
-        # Desigualdades de nu barra bajo la transformaci'on
-    
         
         q2=min(nu1,nu1+l1-mu1,nu1+l2-mu2)        
         
 
+        if g1 - g0 < 0 : print "La region P2 ES VACIA! (límite derecho es negativo)"
+
         
-        #REGION P2
-        g0 = max(0, l2-mu1)
-        g1 =min(l1-mu1, l2-mu2)
+
         ineqs = [x>=g0, x <= g1 , y >= nu1/3, x+2*y <= nu1, x+y <= mu2,  x+y >= mu3, y-x >= nu1-l2, 2*y-x>=nu1-mu3]
-        
-        #REGION P1 cap P2
-        #b1 = min(mu2,nu1/2)
-        #b0 = max(nu-l2, (nu1-mu3)/2, nu1/3,mu3)
-        
+
         G=region_plot(ineqs, (x,xmin,xmax), (y,ymin,ymax),incol="blue")
         
         eqs =  [ x==g0, x == g1 , y == nu1/3, x+2*y == nu1, x+y == mu2,  x+y == mu3]
@@ -119,7 +113,6 @@ def regionP2(l1,l2,mu1,mu2,mu3,nu1,nu2):
         for eq in eqs:
             G+=implicit_plot(eq, (x,xmin,xmax), (y,ymin,ymax),color="blue")
 
-            
         puntos=[(i,j) for i in range(xmin,xmax+1) for j in range(ymin,ymax+1)]
         #for p in puntos:
         #    G+=point(p,size=40)
@@ -212,8 +205,7 @@ def regiones(        l1,l2,mu1,mu2,mu3,nu1,nu2):
     print "lambda: [",l1,l2,"]"
     print "mu:     [",mu1,mu2,mu3,"]"
     print "nu:     [",nu1,nu2,"]"
-    
-    
+        
     if (mu1 < mu2) or (mu2 < mu3):
         print("mu no es una partición")
     elif not(mu1+mu2+mu3 == nu1+nu2):
@@ -234,6 +226,9 @@ def regiones(        l1,l2,mu1,mu2,mu3,nu1,nu2):
         show(G1)
         show(G2)
         
-        print "interseccion:", floor(b1)-ceil(b0)+1
+        if (l1 >= mu1) and (mu1 >= l2) and (l2 >= mu2):
+            print "interseccion:", floor(b1)-ceil(b0)+1
+        else:
+            print "intersección: 0", "(fallan condiciones de orden)"
         
         show(Gb)
